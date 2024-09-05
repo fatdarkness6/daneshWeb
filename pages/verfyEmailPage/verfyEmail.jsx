91ecf5f17b0968ecfdd0515ac494b2e34d7c7898
arsamsarkhosh <arsamsteam70@gmail.com>
2024-09-05 19:23:51 +0330

    login test

--- pages/verfyEmailPage/verfyEmail.jsx
+++ pages/verfyEmailPage/verfyEmail.jsx
@@ -1,6 +1,6 @@
 import { useEffect , useState } from "react";
 import { useParams } from "react-router-dom"
-import { verfyEmailAdress } from "../../api/verfyEmail";
+import { verfyEmailAdress } from "../../api/VerfyEmail";
 
 export default function VerfyEmailPage() {
 
